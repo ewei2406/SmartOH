dcc4e83017c504ec728beecbce01f769a9f12e07
jackomohundro <jackomohundro21@gmail.com>
2023-09-16 15:05:06 -0400

    added TA buttons and logic

--- SmartOH/src/components/TAComponent.tsx
+++ SmartOH/src/components/TAComponent.tsx
@@ -1,15 +1,26 @@
 // src/TAComponent.tsx
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const TAComponent: React.FC = () => {
+  const navigate = useNavigate()
+const newQueue = (className: string) =>  {
+  navigate(`queue/${className}`)
+}
+const classes = ['Math 101', 'History 201', 'Computer Science 301'];
+
   return (
     <div>
       <h2>TA Dashboard</h2>
       <h3>Classes you assist:</h3>
       <ul>
-        <li>Physics 102</li>
-        <li>Chemistry 202</li>
-        <li>Biology 303</li>
+        {classes.map((className, index) => (
+            <li key={index}>
+              <button onClick={() => newQueue(className)}>
+                Go to {className} Office Hours Queue
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   );
