0f729f86480d7a9fdb0e499f450ca3d38a6c2271
Ravi J <spy7sw@virginia.edu>
2023-09-16 12:46:59 -0400

    xd

---
+++ SmartOH/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <div className='font-bold'>hello world</div>
+  )
+}
+
+export default App
---
+++ SmartOH/src/components/login.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { useState } from "react"
+
+export const Login  = () => {
+
+    let [userName, setUsername] = useState<string>();
+    let [password, setPassword] = useState<string>();
+
+    return (
+        <div></div>
+    )
+}
